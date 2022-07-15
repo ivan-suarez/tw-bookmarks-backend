d4ba1e586b8ac8f9d715e8063f28544754b7ce2d
ivan-suarez <ivansuarezcast@gmail.com>
2022-07-15 11:03:22 +0000

    Implement login method

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express")
 const app = express()
 const Cors = require('cors')
 const port = 8081
+const dotenv = require('dotenv')
+dotenv.config()
 
 app.use(Cors())
 
@@ -43,7 +45,7 @@ async function input(prompt) {
 // export CLIENT_ID='YOUR-CLIENT-ID'
 // export CLIENET_SECRET='YOUR-CLIENT-SECRET'
 const CLIENT_ID = `${process.env.CLIENT_ID}`
-const CLIENT_SECRET = `${process.env.CLIENT_SECRET}`;
+const CLIENT_SECRET = `${process.env.CLIENT_SECRET}`
 
 // Optional parameters for additional payload data
 const params = {
@@ -55,6 +57,29 @@ const params = {
 app.get('/', (req, res) => {
     res.send('Hello World!')
   });
+
+app.get('/login', (req, res) => {
+    const authClient = new auth.OAuth2User({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        callback: "https://www.example.com/oauth",
+        scopes: ["tweet.read", "users.read", "bookmark.read"],
+    });
+    
+    const client = new Client(authClient);
+    const STATE = "my-state";
+      
+    //Get authorization
+    //Step 1: Construct an Authorize URL
+    const authUrl = authClient.generateAuthURL({
+    state: STATE,
+      code_challenge: "challenge",
+    });
+    
+    console.log(`Please go here and authorize:`, authUrl);
+
+    res.send(authUrl);
+});
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
